4bc2c59bb556c686e91bc3ad69d1e2f3773e2927
Dev Detour <devdetour@gmail.com>
2025-04-26 00:31:23 -0700

    i can't believe you've done this

--- src/load_data.ts
+++ src/load_data.ts
@@ -1,9 +1,9 @@
 export async function loadAllIds(): Promise<any> {
-    return loadData('/youtube-id-words/all_ids.json')
+    return loadData('/all_ids.json')
 }
 
 export async function loadWordsToIds(): Promise<any> {
-    return loadData('/youtube-id-words/words_to_ids.json')
+    return loadData('/words_to_ids.json')
 }
 
 async function loadData(path: string): Promise<any> {
